f58e7f68b77f5471c9f2e097c0294aee512ce722
Nazira Usenkanova <nusenkanova@gmail.com>
2019-03-27 08:11:31 -0700

    server

--- server/server.js
+++ server/server.js
@@ -1,26 +1,28 @@
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
 const express = require("express");
+const dbConfig = require("./config/database.conf");
 
 const app = express();
 
+//Parse requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const port = 3001;
 
-mongoose.connect("mongodb://localhost:27017/local");
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(dbConfig.url, { useNewUrlParser: true })
+  .then(() => console.log("Successfuly connected"))
+  .catch("Failed to connect", error => {
+    console.log(error);
+    process.exit();
+  });
 
 const db = mongoose.connection;
 
-//Catch error
-db.on("error", console.log("Database connecting error"));
-
-function startServer() {
-  console.log("Connected to database");
-  app.get(
-    "/",
-    (req, res) => res.sendFile(path.join(__dirname)),
-    "../client/public/index.html"
-  );
-  app.listen(port, () => console.log(`Server is running on port: ${port}`));
-}
-
-db.once("open", startServer());
+console.log("Connected to database");
+app.get("/", (req, res) => res.json({ message: "Welcome!" }));
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
